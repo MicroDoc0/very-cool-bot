e2c9e44c41a7f1c8d6dc4d462b87b83647837184
MicroDoc0 <113009474+MicroDoc0@users.noreply.github.com>
2024-04-08 08:33:18 +0200

    Add files via upload

---
+++ Bot/config.py
@@ -0,0 +1,10 @@
+import hikari
+
+# config.py
+BOT_TOKEN = "BOT TOKEN"  # Replace with your actual bot token
+INTENTS = hikari.Intents.GUILD_MEMBERS | hikari.Intents.GUILD_MESSAGES
+
+mod_id = MOD_ROLE_ID  # Replace with the moderator role ID
+hmod_id = HEAD_MOD_ID  # Replace with the head mod role ID (optional)
+admin_id = ADMIN_ROLE_ID  # Replace with the admin role ID
+
